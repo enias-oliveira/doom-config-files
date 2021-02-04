2650fa19a19d256dafcc833e8f1ec1eeeff63842
Enias Oliveira <eniasoliveira27@gmail.com>
2021-02-04 18:56:06 -0300

    updates personal information

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Enias Oliveira"
+      user-mail-address "eniasoliveira27@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -95,7 +95,6 @@
       "d n" #'neotree-dir)
 
 ;; Python Configuration
-
 (use-package flycheck
   :config
   (setq-default flycheck-disabled-checkers '(python-pylint)))
